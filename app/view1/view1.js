e08e89b7e89f303ccdd0d3d500603bd57093426b
eugene-goldberg <eugene.goldberg117@gmail.com>
2016-04-01 19:23:56 -0500

    changed over to using Gcim OData WebAPI

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,49 +11,86 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope','$odataresource', function($scope, $odataresource) {
 
+    //function getData(){
+    //        $odataresource("http://services.odata.org/V4/Northwind/Northwind.svc/Customers")
+    //            .odata()
+    //            .expand('Orders')
+    //            .query(function(data) {
+    //                //console.log('success');
+    //                //console.log(data);
+    //
+    //                $scope.gridOptions = {
+    //                    dataSource:
+    //                    {
+    //                        store: {
+    //                            type: "array",
+    //                            data: data
+    //                            //$scope.results
+    //                        }
+    //                    }
+    //                    ,
+    //                    columns: [{
+    //                        dataField: "CustomerID",
+    //                        caption: "Customer ID"
+    //                    },
+    //                         {
+    //                            dataField: "CompanyName",
+    //                             caption: "Company Name"
+    //                        },
+    //                        {
+    //                            dataField: "ContactName",
+    //                            caption: "Contact Name"
+    //                        }
+    //                    ],
+    //                    masterDetail: {
+    //                        enabled: true,
+    //                        template: "detail"
+    //                    }
+    //                };
+    //
+    //                angular.forEach(data, function(value, key) {
+    //                    //console.log(value['Category'].CategoryName);
+    //                });
+    //            }, function(err) {
+    //                console.log('There was an error: ', err);
+    //            });
+    //}
+
     function getData(){
-            $odataresource("http://services.odata.org/V4/Northwind/Northwind.svc/Customers")
-                .odata()
-                .expand('Orders')
-                .query(function(data) {
-                    //console.log('success');
-                    //console.log(data);
+        $odataresource("http://windows-10:8080/SubjectArea")
+            .odata()
+            .expand('BusinessEntities')
+            .query(function(data) {
+                //console.log('success');
+                //console.log(data);
 
-                    $scope.gridOptions = {
-                        dataSource:
-                        {
-                            store: {
-                                type: "array",
-                                data: data
-                                //$scope.results
-                            }
+                $scope.gridOptions = {
+                    dataSource:
+                    {
+                        store: {
+                            type: "array",
+                            data: data
+                            //$scope.results
                         }
-                        ,
-                        columns: [{
-                            dataField: "CustomerID",
-                            caption: "Customer ID"
-                        },
-                             {
-                                dataField: "CompanyName",
-                                 caption: "Company Name"
-                            },
-                            {
-                                dataField: "ContactName",
-                                caption: "Contact Name"
-                            }
-                        ],
-                        masterDetail: {
-                            enabled: true,
-                            template: "detail"
-                        }
-                    };
+                    }
+                    ,
+                    columns: [{
+                        dataField: "Name",
+                        caption: "Subject Area"
+                    }
+                    ],
+                    masterDetail: {
+                        enabled: true,
+                        template: "detail"
+                    }
+                };
 
-                    angular.forEach(data, function(value, key) {
-                        //console.log(value['Category'].CategoryName);
-                    });
-                }, function(err) {
-                    console.log('There was an error: ', err);
+                angular.forEach(data, function(value, key) {
+                    //console.log(value['Category'].CategoryName);
                 });
+            }, function(err) {
+                console.log('There was an error: ', err);
+            });
     }
 
         getData();
